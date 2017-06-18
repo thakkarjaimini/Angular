2eb08231fc7f738bb75911648f497775e564466e
thakkarjaimini <thakkarjaimini@gmail.com>
2017-06-17 20:24:38 -0700

    String interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,10 @@ import { Component} from '@angular/core';
     templateUrl:'./server.component.html'
 })
 export class ServerComponent{
+serverId = 10;
+serverStatus: string = 'offline';
+getServerStatus(){
+    return this.serverStatus;
+}
 
 }
